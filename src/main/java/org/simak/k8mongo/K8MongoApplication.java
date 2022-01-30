6df8b6dbdba4e648290cc12589c3134d4f76f5d7
Simak Barbara <barbara.simak@a1.at>
2022-01-30 15:37:20 +0100

    Initial commit

---
+++ src/main/java/org/simak/k8mongo/K8MongoApplication.java
@@ -0,0 +1,13 @@
+package org.simak.k8mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8MongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8MongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/simak/k8mongo/K8MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simak.k8mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8MongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
